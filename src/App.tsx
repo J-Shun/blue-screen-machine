18c407e71844fa97c634c6a9c5ec4c58e5d49dbc
J-Shun <ter0123540@gmail.com>
2025-03-09 20:34:01 +0800

    fix: 修正 yml 內容

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
 
-function Spinner() {
-  return (
-    <div className='dots'>
-      <div className='dot'></div>
-      <div className='dot'></div>
-      <div className='dot'></div>
-      <div className='dot'></div>
-      <div className='dot'></div>
-      <div className='dot'></div>
-    </div>
-  );
-}
+// function Spinner() {
+//   return (
+//     <div className='dots'>
+//       <div className='dot'></div>
+//       <div className='dot'></div>
+//       <div className='dot'></div>
+//       <div className='dot'></div>
+//       <div className='dot'></div>
+//       <div className='dot'></div>
+//     </div>
+//   );
+// }
 
 function BlueScreenContainer() {
   const [progress, setProgress] = useState(0);
@@ -32,7 +31,7 @@ function BlueScreenContainer() {
 
   return (
     <div className='w-full min-h-screen bg-[#0078D7] text-[#EBF3FB] px-[128px] pt-[128px] '>
-      {/* <h1 className='text-[164px]'>:(</h1>
+      <h1 className='text-[164px]'>:(</h1>
       <p className='text-3xl mb-4'>您的裝置發生問題，因此必須重新啟動。</p>
       <p className='text-3xl mb-8'>
         我們剛剛正在收集某些錯誤資訊，接著您可以重新啟動
@@ -43,13 +42,13 @@ function BlueScreenContainer() {
       </p>
       <p className='text-1xl mb-2'>致電支援人員時，請提供此資訊:</p>
       <p className='text-1xl mb-2'>停止代碼: 0x0000005</p>
-      <p className='text-1xl'>失敗的項目: win32kbase.sys</p> */}
+      <p className='text-1xl'>失敗的項目: win32kbase.sys</p>
 
-      <div className='text-2xl flex flex-col items-center gap-2'>
+      {/* <div className='text-2xl flex flex-col items-center gap-2'>
         <Spinner />
         <p>正在處理更新 XX%</p>
         <p>請勿關閉電腦，這需要一些時間</p>
-      </div>
+      </div> */}
     </div>
   );
 }
