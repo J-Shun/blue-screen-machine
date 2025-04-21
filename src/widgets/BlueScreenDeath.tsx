ad694a428d64c137ca5cc64a6143e8cefc7743e9
Shun_Tseng <shun_tseng@migocorp.com>
2025-04-21 17:02:01 +0800

    feat: 針對藍屏追加 i18n、修正 typo、修改 Mac icon

--- src/widgets/BlueScreenDeath.tsx
+++ src/widgets/BlueScreenDeath.tsx
@@ -1,20 +1,23 @@
+import { useTranslation } from "react-i18next";
+
 const BlueScreenDeath = ({ progress }: { progress: number }) => {
+  const { t, i18n } = useTranslation();
+
   const progressInt = Math.floor(progress);
 
   return (
     <div className='flex-1 bg-[#0078D7] text-[#EBF3FB] px-[128px] pt-[128px] '>
-      <h1 className='text-[164px]'>:(</h1>
-      <p className='text-3xl mb-4'>您的裝置發生問題，因此必須重新啟動。</p>
+      {i18n.language !== "ja" && <p className='text-[164px]'>:(</p>}
+
+      <p className='text-3xl mb-4'>{t("blueScreen.info")}</p>
+      <p className='text-3xl mb-8'>{t("blueScreen.gatherError")}</p>
       <p className='text-3xl mb-8'>
-        我們剛剛正在收集某些錯誤資訊，接著您可以重新啟動
+        {t("blueScreen.complete", { progress: progressInt })}
       </p>
-      <p className='text-3xl mb-8'>已完成 {progressInt}%</p>
-      <p className='text-1xl mb-8'>
-        如需此問題與可能修正的詳細資訊，請瀏覽 https://www.windows.com/stopcode
-      </p>
-      <p className='text-1xl mb-2'>致電支援人員時，請提供此資訊:</p>
-      <p className='text-1xl mb-2'>停止代碼: 0x0000005</p>
-      <p className='text-1xl'>失敗的項目: win32kbase.sys</p>
+      <p className='text-1xl mb-8'>{t("blueScreen.detail")}</p>
+      <p className='text-1xl mb-2'>{t("blueScreen.needInfo")}</p>
+      <p className='text-1xl mb-2'>{t("blueScreen.errorCode")} 0x0000005</p>
+      <p className='text-1xl'>{t("blueScreen.failedData")} win32kbase.sys</p>
     </div>
   );
 };
