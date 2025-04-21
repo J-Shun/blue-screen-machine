cb437fb35467cd042d6eff0b5642270955b7a4cb
J-Shun <ter0123540@gmail.com>
2025-04-21 17:53:01 +0800

    style: 調整排版

--- src/widgets/BlueScreenDeath.tsx
+++ src/widgets/BlueScreenDeath.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 const BlueScreenDeath = ({ progress }: { progress: number }) => {
   const { t, i18n } = useTranslation();
@@ -6,18 +6,20 @@ const BlueScreenDeath = ({ progress }: { progress: number }) => {
   const progressInt = Math.floor(progress);
 
   return (
-    <div className='flex-1 bg-[#0078D7] text-[#EBF3FB] px-[128px] pt-[128px] '>
-      {i18n.language !== "ja" && <p className='text-[164px]'>:(</p>}
+    <div className='flex-1 bg-[#0078D7] text-[#EBF3FB] px-[128px] flex flex-col justify-center'>
+      <div className='w-1/2'>
+        {i18n.language !== 'ja' && <p className='text-[164px]'>:(</p>}
 
-      <p className='text-3xl mb-4'>{t("blueScreen.info")}</p>
-      <p className='text-3xl mb-8'>{t("blueScreen.gatherError")}</p>
-      <p className='text-3xl mb-8'>
-        {t("blueScreen.complete", { progress: progressInt })}
-      </p>
-      <p className='text-1xl mb-8'>{t("blueScreen.detail")}</p>
-      <p className='text-1xl mb-2'>{t("blueScreen.needInfo")}</p>
-      <p className='text-1xl mb-2'>{t("blueScreen.errorCode")} 0x0000005</p>
-      <p className='text-1xl'>{t("blueScreen.failedData")} win32kbase.sys</p>
+        <p className='text-3xl mb-4 leading-[1.75]'>{t('blueScreen.info')}</p>
+        <p className='text-3xl mb-8'>{t('blueScreen.gatherError')}</p>
+        <p className='text-3xl mb-8'>
+          {t('blueScreen.complete', { progress: progressInt })}
+        </p>
+        <p className='text-1xl mb-8'>{t('blueScreen.detail')}</p>
+        <p className='text-1xl mb-2'>{t('blueScreen.needInfo')}</p>
+        <p className='text-1xl mb-2'>{t('blueScreen.errorCode')} 0x0000005</p>
+      </div>
+      <p className='text-1xl'>{t('blueScreen.failedData')} win32kbase.sys</p>
     </div>
   );
 };
