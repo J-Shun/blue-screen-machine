706bd6a2b55c8b2125b50e13d13460ef62da7bb5
Shun_Tseng <shun_tseng@migocorp.com>
2025-04-22 10:57:08 +0800

    feat: 更新畫面新增 i18n

--- src/widgets/BlueScreenUpdate.tsx
+++ src/widgets/BlueScreenUpdate.tsx
@@ -1,13 +1,15 @@
 import Spinner from "./Spinner";
+import { useTranslation } from "react-i18next";
 
 const BlueScreenUpdate = ({ progress }: { progress: number }) => {
+  const { t } = useTranslation();
   const progressInt = Math.floor(progress);
   return (
     <div className='flex-1 bg-[#0078D7] text-[#EBF3FB] text-2xl flex flex-col items-center justify-center gap-2'>
       <Spinner />
-      <p>正在處理更新</p>
-      <p>{progressInt}% 完成</p>
-      <p>請勿關閉電腦</p>
+      <p>{t("windowsUpdate.info")}</p>
+      <p>{t("windowsUpdate.progress", { progress: progressInt })}</p>
+      <p>{t("windowsUpdate.warning")}</p>
     </div>
   );
 };
